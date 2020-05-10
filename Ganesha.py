4efe12d050a6c2fa3350bc3878fea672784995a4
MohitS1999 <49729753+MohitS1999@users.noreply.github.com>
2020-05-10 16:01:05 +0530

    Update Ganesha.py

--- Ganesha.py
+++ Ganesha.py
@@ -1,5 +1,6 @@
 def pattern(n):
     a=n//2-1
+    #for upper block
     for i in range(n):
         if i<n//2:
             if i==0:
@@ -13,9 +14,11 @@ def pattern(n):
                 for j in range(a):
                     print(' ',end='')
                 print('*',end='')
+        #For Mid Line
         elif i==n//2:
-            for j in range(n):
+            for j in range(n+1):
                 print('*',end='')
+        #for Lower Block
         elif i>n//2:
             if i==n-1:
                 for j in range(n//2+1):
@@ -32,3 +35,15 @@ def pattern(n):
                 print('*',end='')
         print()
 pattern(int(input()))
+
+##  *    ******
+##  *    *
+##  *    *
+##  *    *
+##  *    *
+##  ***********
+##       *    *
+##       *    *
+##       *    *
+##  ******    *
+
