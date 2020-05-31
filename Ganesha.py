bed4b59485fd809349ab9fca3b83d1dd1593ee24
MohitS1999 <49729753+MohitS1999@users.noreply.github.com>
2020-05-31 20:53:26 +0530

    Ganesha Pattern
    if N is even

--- Ganesha.py
+++ Ganesha.py
@@ -1,49 +1,84 @@
+#For n is EVEN
 def pattern(n):
+
     a=n//2-1
+
     #for upper block
-    for i in range(n):
+
+    for i in range(n+1):
+
         if i<n//2:
+
             if i==0:
+
                 print('*',end='')
+
                 for j in range(a):
+
                     print(' ',end='')
+
                 for j in range(n//2+1):
+
                     print('*',end='')
+
             else:
+
                 print('*',end='')
+
                 for j in range(a):
+
                     print(' ',end='')
+
                 print('*',end='')
+
         #For Mid Line
+
         elif i==n//2:
+
             for j in range(n+1):
+
                 print('*',end='')
+
         #for Lower Block
+
         elif i>n//2:
-            if i==n-1:
+
+            if i==n:
+
                 for j in range(n//2+1):
                     print('*',end='')
+
                 for j in range(a):
+
                     print(' ',end='')
+
                 print('*',end='')
+
             else:
+
                 for j in range(a+1):
-                    print(' ',end='')
+                
+                   print(' ',end='')
+
                 print('*',end='')
+
                 for j in range(a):
+
                     print(' ',end='')
+
                 print('*',end='')
+
         print()
+
 pattern(int(input()))
-
-##  *    ******
-##  *    *
-##  *    *
-##  *    *
-##  *    *
-##  ***********
-##       *    *
-##       *    *
-##       *    *
-##  ******    *
-
+##*    ******
+##*    *
+##*    *
+##*    *
+##*    *
+##***********
+##     *    *
+##     *    *
+##     *    *
+##     *    *
+##******    *
